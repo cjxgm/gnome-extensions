3b8314e96ecf0bcef45c3c5debc97835529e261d
Giumo X. Clanjor (哆啦比猫/兰威举) <cjxgm@126.com>
2015-08-25 14:40:49 +0800

    use init instead of enable/disable.

--- extension.js
+++ extension.js
@@ -1,8 +1,6 @@
 
 const EXT = imports.misc.extensionUtils.getCurrentExtension();
 const ISO_CLOCK = EXT.imports['iso-clock'];
-const iso_clock = ISO_CLOCK.make_iso_clock();
 
-function enable() { iso_clock.enable() }
-function disable() { iso_clock.disable() }
+function init() { return ISO_CLOCK.make_iso_clock() }
 
