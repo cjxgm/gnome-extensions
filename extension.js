0f70be04c1f31e0bd92ac9bf0d371219c46ccf41
Giumo X. Clanjor (哆啦比猫/兰威举) <cjxgm@126.com>
2015-08-26 20:47:41 +0800

    indicator working

--- extension.js
+++ extension.js
@@ -2,6 +2,7 @@
 const EXT = imports.misc.extensionUtils.getCurrentExtension();
 const make_capslip = EXT.imports.capslip.make;
 const make_rippler = EXT.imports.rippler.make;
+const make_indicator = EXT.imports.indicator.make;
 
-function init() { return make_capslip(make_rippler(3)) }
+function init() { return make_capslip(make_rippler(3), make_indicator()) }
 
---
+++ indicator.js
@@ -0,0 +1,73 @@
+
+const ST = (function() {
+	let _ = imports.gi.St;
+	let $ = {};
+	$.bin = _.Bin;
+	return $;
+})();
+
+const MAIN = (function() {
+	let _ = imports.ui.main;
+	let $ = {};
+	$.ui      = _.uiGroup;
+	$.monitor = _.layoutManager.primaryMonitor;
+	return $;
+})();
+
+const tween = imports.ui.tweener.addTween;
+
+
+function make()
+{
+	let get_css_height = function(actor) {
+		return actor.peek_theme_node().get_height();
+	}
+
+	let make_indicator = function(parent, opacity_init, next) {
+		let indicator = new ST.bin({
+			style_class: 'capslip-indicator',
+			x: 0,
+			y: 0, 	// will be calculated later
+			opacity: opacity_init * 255,
+		});
+		parent.add_actor(indicator);
+
+		indicator._alpha = indicator.opacity;
+		indicator.y = (MAIN.monitor.height - get_css_height(indicator)) / 2;
+
+		tween(indicator, {
+			_alpha: 0,
+			time: 0.4,
+			transition: 'easeOutQuad',
+			onUpdate: function() { indicator.opacity = indicator._alpha; },
+			onComplete: function() {
+				parent.remove_actor(indicator);
+				next();
+			},
+		});
+	}
+
+	let indicating;
+	let indicate_request;
+
+	let do_indicate = function() {
+		if (indicate_request !== undefined) {
+			indicating = indicate_request;
+			indicate_request = undefined;
+		}
+		if (!indicating) {
+			indicating = undefined;
+			return;
+		}
+
+		make_indicator(MAIN.ui, 0.7, do_indicate);
+	}
+
+	let request_indicate = function(enable) {
+		indicate_request = enable;
+		if (indicating === undefined) do_indicate();
+	}
+
+	return request_indicate;
+}
+
