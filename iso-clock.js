4a0c49e5dc7301c0acc6449b7bf2d332582094b5
Giumo X. Clanjor (哆啦比猫/兰威举) <cjxgm@126.com>
2015-08-30 17:45:04 +0800

    tidy up: orig_clock -> old_clock

--- iso-clock.js
+++ iso-clock.js
@@ -34,8 +34,8 @@ function make()
 {
 	const clock_container = MAIN.panel._centerBox.get_child_at_index(0).get_child_at_index(0).get_child_at_index(0);
 	const clock_index = 1;		// TODO: find a better way instead of hard-coding this
-	const orig_clock = clock_container.get_child_at_index(clock_index);
-	const  iso_clock = new ST.box({ style_class: 'iso-clock' });
+	const old_clock = clock_container.get_child_at_index(clock_index);
+	const iso_clock = new ST.box({ style_class: 'iso-clock' });
 	const label_day  = new ST.label({ style_class: 'iso-clock-day' , y_align: ACTOR_ALIGN.center });
 	const label_date = new ST.label({ style_class: 'iso-clock-date', y_align: ACTOR_ALIGN.center });
 	const label_time = new ST.label({ style_class: 'iso-clock-time', y_align: ACTOR_ALIGN.center });
@@ -65,7 +65,7 @@ function make()
 
 	$.enable = function() {
 		update_clock();
-		clock_container.remove_child(orig_clock);
+		clock_container.remove_child(old_clock);
 		clock_container.insert_child_at_index(iso_clock, clock_index);
 		timer = MAINLOOP.timeout_add(1000, update_clock);
 	}
@@ -73,7 +73,7 @@ function make()
 	$.disable = function() {
 		MAINLOOP.source_remove(timer);
 		clock_container.remove_child(iso_clock);
-		clock_container.insert_child_at_index(orig_clock, clock_index);
+		clock_container.insert_child_at_index(old_clock, clock_index);
 	}
 
 	return $;
