bfe452d6d3afe34a028bd762230d1884fe5201bb
Giumo Clanjor (哆啦比猫/兰威举) <cjxgm2@gmail.com>
2019-01-26 19:15:48 +0800

    Tune parameters

--- extension.js
+++ extension.js
@@ -3,7 +3,7 @@ const EXT = imports.misc.extensionUtils.getCurrentExtension();
 const make_top_fence = EXT.imports['top-fence'].make;
 const make_singleton = EXT.imports.singleton.make;
 const make_barrier = EXT.imports.barrier.make(make_singleton);
-const make_fence = EXT.imports.fence.make(50, 20, 400, make_barrier, make_singleton);
+const make_fence = EXT.imports.fence.make(20, 18, 40, make_barrier, make_singleton);
 
-function init() { return make_top_fence(14, make_fence, make_singleton) }
+function init() { return make_top_fence(44, make_fence, make_singleton) }
 
